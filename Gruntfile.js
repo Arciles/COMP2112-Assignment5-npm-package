5c8a77e9525c328b2cb12ae5e34aefa04d7489b2
IBIS, Esat <esatibis@expertech.net>
2016-12-05 13:17:00 -0500

    Build process automated using grunt and other tools.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,31 @@
 // Project configuration.
-grunt.initConfig({
-  pkg: grunt.file.readJSON('package.json'),
-  uglify: {
-    options: {
-      banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+'use strict';
+module.exports = function(grunt){
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+        mangle: true
+      },
+      build: {
+        src: 'build/chatbot.js',
+        dest: 'demo/build/<%= pkg.name %>.min.js'
+      }
     },
-    build: {
-      src: 'src/<%= pkg.name %>.js',
-      dest: 'build/<%= pkg.name %>.min.js'
+    copy: {
+      files: {
+        cwd: '/build',                       // set working folder / root to copy
+        src: 'build', // copy all files and subfolders
+        dest: 'demo/build',                  // destination folder
+        expand: true                         // required when using cwd
+      }
     }
-  }
-});
+  });
+  //grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.registerTask('default', function(target) {
+    grunt.task.run('uglify');
+    grunt.task.run('copy');
+  });
+};
