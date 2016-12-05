9b01aee87a0e1ccfda98762c7f09114d1cd310ce
IBIS, Esat <esatibis@expertech.net>
2016-12-05 15:04:45 -0500

    Entire build process is handled bt grunt now

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,27 +4,35 @@ module.exports = function(grunt){
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    browserify: {
+     vendor: {
+       src: 'src/index.js',
+       dest: 'build/index.js'
+     }
+    },
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
         mangle: true
       },
       build: {
-        src: 'build/chatbot.js',
-        dest: 'demo/build/<%= pkg.name %>.min.js'
+        src: 'build/index.js',
+        dest: 'build/index.min.js'
       }
     },
     copy: {
-      files: {
-        cwd: '/build',                       // set working folder / root to copy
-        src: 'build', // copy all files and subfolders
-        dest: 'demo/build',                  // destination folder
-        expand: true                         // required when using cwd
+      main: {
+        files: [
+            {expand: true,
+               src: ['build/*'],
+               dest: 'demo/build/',
+               flatten: true}
+        ]
       }
     }
   });
-  //grunt.loadNpmTasks("grunt-contrib-uglify");
   grunt.registerTask('default', function(target) {
+    grunt.task.run('browserify');
     grunt.task.run('uglify');
     grunt.task.run('copy');
   });
