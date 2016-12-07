5f18cd6770cd29b642e25198e5656098453fb38b
IBIS, Esat <esatibis@expertech.net>
2016-12-07 13:32:17 -0500

    some changes made it to the chatbot and test

--- src/chatbot.js
+++ src/chatbot.js
@@ -1,9 +1,40 @@
-function chatbot(message) {
-    if (typeof message === 'undefined') {
-        return 'Hello.'
-    }
+function chatbot(io) {
+  function match(regex) {
 
-    return "Sorry, I don't understand."
+    return new RegExp(regex).test(message);
+  }
+	 // current user input
+	 var message = io;
+   if (message !== null && message !== undefined) {
+
+     message = message.toLowerCase();
+
+     if(match("(hi|hello|hey|hola|howdy)(\\s|!|\\.|$)"))
+       return "hello!!";
+
+     if(match('what[^ ]* up') || match('sup') || match('how are you'))
+       return "this github thing is pretty cool, huh?";
+
+     if(match('l(ol)+') || match('(ha)+(h|$)') || match('lmao'))
+       return "what's so funny?";
+
+     if(match('^no+(\\s|!|\\.|$)'))
+       return "don't be such a negative nancy :(";
+
+     if(match('(cya|bye|see ya|ttyl|talk to you later)'))
+       return ["alright, see you around", "good teamwork!"];
+
+     if(match('(dumb|stupid|is that all)'))
+       return ["hey i'm just a proof of concept", "you can make me smarter if you'd like"];
+
+     if(message == 'noop')
+       return;
+   }
+   return "Sorry, I don't understand.";
+
+
 }
 
-module.exports = chatbot
+
+
+module.exports = chatbot;
--- test/chatbot-test.js
+++ test/chatbot-test.js
@@ -3,20 +3,17 @@ var chai = require('chai')
 var assert = chai.assert;
 var expect = chai.expect;
 
-it('should return a string', function () {
-    var result = chatbot()
+it("should return a string", function () {
+    var result = chatbot();
+    expect(result).to.be.a("string");
+});
 
-    expect(result).to.be.a('string')
-})
+it("should return 'Sorry, I don't understand.' when message is not understood", function () {
+    var result = chatbot("you dont understand this");
+    expect(result).to.equal("Sorry, I don't understand.");
+});
 
-it('should return Hello. when message is undefined', function () {
-    var result = chatbot()
-
-    expect(result).to.equal('Hello.')
-})
-
-it('should return "Sorry, I don\'t understand." when message is not understood', function () {
-    var result = chatbot('you dont understand this')
-
-    expect(result).to.equal('Sorry, I don\'t understand.')
-})
+it("should return 'hello!!' when you greet him", function() {
+  var result = chatbot("hello");
+  expect(result).to.equal("hello!!");
+});
